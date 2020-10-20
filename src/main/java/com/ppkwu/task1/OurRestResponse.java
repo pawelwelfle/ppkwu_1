90bef75e48b8446bc4a7226a442c9ab0d01b4899
Paweł Welfle <pawel.welfle@gmail.com>
2020-10-20 14:46:02 +0200

    prepare method

--- src/main/java/com/ppkwu/task1/OurRestResponse.java
+++ src/main/java/com/ppkwu/task1/OurRestResponse.java
@@ -1,13 +1,15 @@
 package com.ppkwu.task1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author pawel.welfle
  */
-
 @RestController
 public class OurRestResponse {
 
-
+    @RequestMapping("/reverse")
+    public void revString(){
+    }
 }
