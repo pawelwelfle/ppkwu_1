4b41be00691825d48a1b35254e12b43d7a30461d
Paweł Welfle <pawel.welfle@gmail.com>
2020-10-20 14:41:05 +0200

    update maven dependencies and add adnotation

--- src/main/java/com/ppkwu/task1/OurRestResponse.java
+++ src/main/java/com/ppkwu/task1/OurRestResponse.java
@@ -1,8 +1,13 @@
 package com.ppkwu.task1;
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author pawel.welfle
  */
 
+@RestController
 public class OurRestResponse {
+
+
 }
