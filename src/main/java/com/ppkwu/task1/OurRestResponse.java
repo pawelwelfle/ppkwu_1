f22981a686278974d9a5cf6a60ffc7241fa5b247
Paweł Welfle <pawel.welfle@gmail.com>
2020-10-20 15:04:21 +0200

    update method

--- src/main/java/com/ppkwu/task1/OurRestResponse.java
+++ src/main/java/com/ppkwu/task1/OurRestResponse.java
@@ -1,6 +1,7 @@
 package com.ppkwu.task1;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OurRestResponse {
 
-    @RequestMapping("/reverse")
+    @GetMapping("/reverse")
+    @ResponseBody
     public String revString() {
         String word = "Test";
         return word;
