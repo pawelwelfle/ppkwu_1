835bbaa3f3ad27a486f6a22c7500a7f7478d249b
Paweł Welfle <pawel.welfle@gmail.com>
2020-10-20 15:20:30 +0200

    change method, use Stringbuilder to use reverse

--- src/main/java/com/ppkwu/task1/OurRestResponse.java
+++ src/main/java/com/ppkwu/task1/OurRestResponse.java
@@ -1,5 +1,6 @@
 package com.ppkwu.task1;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,10 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class OurRestResponse {
     /**
      * to test the method we must hit to the endpoint --->  http://localhost:8080/reverse?testedWord=testowanyString
-     * */
+     */
     @GetMapping("/reverse")
     @ResponseBody
     public String revString(@RequestParam String testedWord) {
+        StringBuilder stringBuilder = new StringBuilder(testedWord).reverse();
+        testedWord = stringBuilder.toString();
         return testedWord;
     }
 }
